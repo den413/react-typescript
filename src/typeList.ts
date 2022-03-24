b820ece0446b5efbd1aa938da3ef4c82f41e9b15
den413 <92449901+den413@users.noreply.github.com>
2022-03-25 00:03:18 +0900

    type apply

---
+++ src/typeList.ts
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 9;
+
+// string
+let st: string = "q";
+
+// array
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1.2];
+
+// tuple
+let tuple: [number, string] = [0, "a"];
+
+// any
+let any1: any = false;
+
+// void
+const funcA = (): void => {
+  const test = "s";
+};
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// objct
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "aaa" };
